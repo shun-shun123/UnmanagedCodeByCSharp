5add1efc482b10374da3a8aca41d88704b7d7bf7
shun-shun123 <shunsuke15vb@gmail.com>
2021-08-01 19:04:16 +0900

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnmanagedSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
